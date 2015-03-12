0331a3caa3d916c728a7fadc22b7b96e591c7d79
nguyenthanhtrung <duongbata123@gmail.com>
2015-03-12 23:51:23 +0700

    commit

--- src/main/java/com/crawler/Main.java
+++ src/main/java/com/crawler/Main.java
@@ -4,12 +4,18 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Document doc = Jsoup.connect("http://trollbongda.com/?start=40").get();
-		Elements es = doc.getElementsByClass("itemContainer");
-		System.out.println(es.size());
+		String linkDoc = "http://trollbongda.com";
+		Document doc = Jsoup.connect(linkDoc + "/?start=40").get();
+		Elements es = doc.getElementsByClass("catItemTitle");
+		for (Element e :es) {
+			Element tagA = e.getElementsByTag("a").first();
+			String link = tagA.attr("href");
+			System.out.println(linkDoc+link);
+		}
 	}
 }
